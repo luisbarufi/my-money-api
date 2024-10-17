44395b38eb5822fba0d2504af8606902d743c88f
Luis Barufi <luisbarufi@gmail.com>
2024-10-16 21:09:23 -0300

    Creating User Objects

---
+++ src/controller/model/request/user_request.go
@@ -0,0 +1,11 @@
+package request
+
+import "time"
+
+type UserRequest struct {
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
---
+++ src/controller/model/response/user_response.go
@@ -0,0 +1,11 @@
+package response
+
+import "time"
+
+type UserResponse struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
