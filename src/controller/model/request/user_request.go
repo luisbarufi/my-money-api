65d4627eea02710682c55e598962786174a8e03c
Luis Barufi <luisbarufi@gmail.com>
2024-10-16 21:44:47 -0300

    Validating request fields

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -3,9 +3,9 @@ package request
 import "time"
 
 type UserRequest struct {
-	Name      string    `json:"name"`
-	Email     string    `json:"email"`
-	Password  string    `json:"password"`
+	Name      string    `json:"name" binding:"required,min=5,max=100"`
+	Email     string    `json:"email" binding:"required,email"`
+	Password  string    `json:"password" binding:"required,min=5,containsany=!@#$%&*"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
