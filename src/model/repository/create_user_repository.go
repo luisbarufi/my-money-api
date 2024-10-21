dbbc3b4d72f504cfcdff33a50e283098b6a17b2a
Luis Barufi <luisbarufi@gmail.com>
2024-10-20 23:27:31 -0300

    Refactoring the CreateUser repository

--- src/model/repository/create_user_repository.go
+++ src/model/repository/create_user_repository.go
@@ -15,32 +15,53 @@ func (ur *userRepository) CreateUser(userDomain model.UserDomainInterface) (
 	model.UserDomainInterface, *rest_err.RestErr,
 ) {
 	logger.Info("Init createUser repository", zap.String("journey", "createUser"))
-	query := `INSERT INTO users (name, nick, email, password)
-						VALUES ($1, $2, $3, $4) 
-						RETURNING id, name, nick, email, password, created_at, updated_at`
 
+	query, args := buildInsertUserQuery(userDomain)
+
+	user, err := ur.executeInsertUser(query, args)
+	if err != nil {
+		return nil, err
+	}
+
+	logger.Info("CreateUser repository executed successfully",
+		zap.String("userId", fmt.Sprintf("%d", user.ID)),
+		zap.String("journey", "createUser"),
+	)
+
+	return converter.ConvertEntityToDomain(*user), nil
+}
+
+func buildInsertUserQuery(userDomain model.UserDomainInterface) (
+	string, []interface{},
+) {
+	query := `INSERT INTO users (name, nick, email, password)
+	          VALUES ($1, $2, $3, $4) 
+	          RETURNING id, name, nick, email, password, created_at, updated_at`
 	value := converter.ConvertDomainToEntity(userDomain)
 
-	row, err := ur.db.Conn.Query(
-		query,
+	args := []interface{}{
 		value.Name,
 		value.Nick,
 		value.Email,
 		value.Password,
-	)
+	}
+
+	return query, args
+}
+
+func (ur *userRepository) executeInsertUser(query string, args []interface{}) (
+	*entity.UserEntity, *rest_err.RestErr,
+) {
+	row, err := ur.db.Conn.Query(query, args...)
 	if err != nil {
-		logger.Error("Error trying to create user",
-			err,
-			zap.String("journey", "createUser"),
-		)
+		logger.Error("Error executing insert user query", err)
 		return nil, rest_err.NewInternalServerError(err.Error())
 	}
 	defer row.Close()
 
 	var user entity.UserEntity
-
 	if row.Next() {
-		if err = row.Scan(
+		if err := row.Scan(
 			&user.ID,
 			&user.Name,
 			&user.Nick,
@@ -49,18 +70,10 @@ func (ur *userRepository) CreateUser(userDomain model.UserDomainInterface) (
 			&user.CreatedAt,
 			&user.UpdatedAt,
 		); err != nil {
-			logger.Error("Error scanning results",
-				err,
-				zap.String("journey", "createUser"),
-			)
+			logger.Error("Error scanning insert user result", err)
 			return nil, rest_err.NewInternalServerError(err.Error())
 		}
 	}
 
-	logger.Info(
-		"CreateUser repository executed successfully",
-		zap.String("userId", fmt.Sprintf("%d", user.ID)),
-		zap.String("journey", "createUser"))
-
-	return converter.ConvertEntityToDomain(user), nil
+	return &user, nil
 }
