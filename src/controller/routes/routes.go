52f9763442c700922e6047d78f06621579231990
Luis Barufi <luisbarufi@gmail.com>
2024-10-26 23:10:32 -0300

    Adds JWT Middleware

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -3,14 +3,15 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/luisbarufi/my-money-api/src/controller"
+	"github.com/luisbarufi/my-money-api/src/model"
 )
 
 func InitRoutes(r *gin.RouterGroup, userController controller.UserControllerInterface) {
-	r.GET("/users/:id", userController.FindUserByIDController)
-	r.GET("/users/email/:email", userController.FindUserByEmailController)
-	r.POST("/users", userController.CreateUserController)
-	r.PUT("/users/:id", userController.UpdateUserController)
-	r.DELETE("/users/:id", userController.DeleteUserController)
+	r.GET("/users/:id", model.VerifyTokenMiddleware, userController.FindUserByIDController)
+	r.GET("/users/email/:email", model.VerifyTokenMiddleware, userController.FindUserByEmailController)
+	r.PUT("/users/:id", model.VerifyTokenMiddleware, userController.UpdateUserController)
+	r.DELETE("/users/:id", model.VerifyTokenMiddleware, userController.DeleteUserController)
 
+	r.POST("/users", userController.CreateUserController)
 	r.POST("/login", userController.LoginUserController)
 }
--- src/model/user_token_domain.go
+++ src/model/user_token_domain.go
@@ -2,10 +2,13 @@ package model
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/luisbarufi/my-money-api/src/configuration/env"
+	"github.com/luisbarufi/my-money-api/src/configuration/logger"
 	"github.com/luisbarufi/my-money-api/src/configuration/rest_err"
 )
 
@@ -34,4 +37,73 @@ func (ud *userDomain) GenerateToken() (string, *rest_err.RestErr) {
 	}
 
 	return tokenString, nil
+}
+
+func VerifyToken(tokenValue string) (UserDomainInterface, *rest_err.RestErr) {
+	secret := env.GetEnv(JWT_SECRET_KEY)
+
+	token, err := jwt.Parse(strings.TrimPrefix(tokenValue, "Bearer "),
+		func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); ok {
+				return []byte(secret), nil
+			}
+
+			return nil, rest_err.NewBadRequestError("Invalid token")
+		},
+	)
+
+	if err != nil {
+		return nil, rest_err.NewUnauthorizedRequestError("Invalid token")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return nil, rest_err.NewUnauthorizedRequestError("Invalid token")
+	}
+
+	return &userDomain{
+		id:    uint64(claims["id"].(float64)),
+		name:  claims["name"].(string),
+		nick:  claims["nick"].(string),
+		email: claims["email"].(string),
+	}, nil
+}
+
+func VerifyTokenMiddleware(c *gin.Context) {
+	secret := env.GetEnv(JWT_SECRET_KEY)
+	tokenValue := strings.TrimPrefix(c.Request.Header.Get("Authorization"), "Bearer ")
+
+	token, err := jwt.Parse(strings.TrimPrefix(tokenValue, "Bearer "),
+		func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); ok {
+				return []byte(secret), nil
+			}
+
+			return nil, rest_err.NewBadRequestError("Invalid token")
+		},
+	)
+
+	if err != nil {
+		errRest := rest_err.NewUnauthorizedRequestError("Invalid token")
+		c.JSON(errRest.Code, errRest)
+		c.Abort()
+		return
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		errRest := rest_err.NewUnauthorizedRequestError("Invalid token")
+		c.JSON(errRest.Code, errRest)
+		c.Abort()
+		return
+	}
+
+	userDomain := userDomain{
+		id:    uint64(claims["id"].(float64)),
+		name:  claims["name"].(string),
+		nick:  claims["nick"].(string),
+		email: claims["email"].(string),
+	}
+
+	logger.Info(fmt.Sprintf("User authenticated: %v", userDomain))
 }
