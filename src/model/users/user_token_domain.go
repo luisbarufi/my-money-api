cb18b4e9e6808df33997fdedb94813cc7ce65d09
Luis Barufi <luisbarufi@gmail.com>
2024-11-13 22:57:41 -0300

    Remove unused function

--- src/model/users/user_token_domain.go
+++ src/model/users/user_token_domain.go
@@ -2,13 +2,10 @@ package model
 
 import (
 	"fmt"
-	"strings"
 	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/luisbarufi/my-money-api/src/configuration/env"
-	"github.com/luisbarufi/my-money-api/src/configuration/logger"
 	"github.com/luisbarufi/my-money-api/src/configuration/rest_err"
 )
 
@@ -61,51 +58,4 @@ func (ud *userDomain) GenerateResetToken() (string, *rest_err.RestErr) {
 	}
 
 	return signedToken, nil
-}
-
-func VerifyTokenMiddleware(c *gin.Context) {
-	secret := env.GetEnv(JWT_SECRET_KEY)
-
-	tokenValue := strings.TrimPrefix(c.Request.Header.Get("Authorization"), "Bearer ")
-
-	token, err := jwt.Parse(strings.TrimPrefix(tokenValue, "Bearer "),
-		func(token *jwt.Token) (interface{}, error) {
-			if _, ok := token.Method.(*jwt.SigningMethodHMAC); ok {
-				return []byte(secret), nil
-			}
-
-			return nil, rest_err.NewBadRequestError("Invalid token")
-		},
-	)
-
-	if err != nil {
-		errRest := rest_err.NewUnauthorizedRequestError("Invalid token")
-
-		c.JSON(errRest.Code, errRest)
-
-		c.Abort()
-
-		return
-	}
-
-	claims, ok := token.Claims.(jwt.MapClaims)
-
-	if !ok || !token.Valid {
-		errRest := rest_err.NewUnauthorizedRequestError("Invalid token")
-
-		c.JSON(errRest.Code, errRest)
-
-		c.Abort()
-
-		return
-	}
-
-	userDomain := userDomain{
-		id:    uint64(claims["id"].(float64)),
-		name:  claims["name"].(string),
-		nick:  claims["nick"].(string),
-		email: claims["email"].(string),
-	}
-
-	logger.Info(fmt.Sprintf("User authenticated: %v", userDomain))
 }
