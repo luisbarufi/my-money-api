efc72350bce6c80aca751a9f73751ceaed27be48
Luis Barufi <luisbarufi@gmail.com>
2024-11-16 17:39:37 -0300

    Remove blank lines

--- src/model/accounts/account_domain_interface.go
+++ src/model/accounts/account_domain_interface.go
@@ -4,27 +4,16 @@ import "time"
 
 type AccountDomainInterface interface {
 	GetID() uint64
-
 	SetID(id uint64)
-
 	GetUserID() uint64
-
 	SetUserID(userID uint64)
-
 	GetAccountName() string
-
 	SetAccountName(accountName string)
-
 	GetBalance() float64
-
 	SetBalance(balance float64)
-
 	GetCreatedAt() time.Time
-
 	SetCreatedAt(created_at time.Time)
-
 	GetUpdatedAt() time.Time
-
 	SetUpdatedAt(updated_at time.Time)
 }
 
